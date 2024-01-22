db46b97cb8042894a3142cce852a2c8f65da1f36
HannaNikola <h.nikolaienko32@gmail.com>
2024-01-22 21:03:00 +0100

    update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,13 +1,14 @@
-import { Formik,  Form } from 'formik';
+
+
+import { useFormik } from 'formik';
 import { useDispatch } from 'react-redux';
+
 import * as Yup from 'yup';
 import { logIn } from '../../redux/auth/operations';
 import InputAdornment from '@mui/material/InputAdornment';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import FormControl from '@mui/material/FormControl';
 import * as React from 'react';
-
-import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import InputLabel from '@mui/material/InputLabel';
 import Visibility from '@mui/icons-material/Visibility';
@@ -18,66 +19,69 @@ import SendIcon from '@mui/icons-material/Send';
 
 
 const LoginUser = Yup.object().shape({
-    name: Yup.string().min(3, 'Too short!').required('This field is required'),
-
+    email: Yup.string().email('Invalid email').required('This field is required'),
+    password: Yup.string().min(6, 'Too short!').required('This field is required'),
 });
 
 
 export const LoginForm = () => {
     const [showPassword, setShowPassword] = React.useState(false);
     const dispatch = useDispatch();
+
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: LoginUser,
+        onSubmit: (values) => {
+            dispatch(logIn(values));
+            formik.resetForm();
+        },
+    });
+
+
+   
+
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
     const handleMouseDownPassword = (event) => {
         event.preventDefault();
     };
-    const handleSubmitLogin = event => {
-        event.preventDefault();
-        const form = event.currentTarget;
 
-        dispatch(
-            logIn({
-                email: form.elements.email.value,
-                password: form.elements.password.value,
-            })
-        );
-        form.reset();
-    }
+   
+
     return (
-        <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
         <div>
 
-            <Formik
-                initialValues={{
-                    email: '',
-                    password: '',
-                }}
-                validationSchema={LoginUser}
+                <form onSubmit={formik.handleSubmit} autoComplete="off" >
+                   
+                    <TextField
+                        fullWidth
+                        id="email"
+                        name="email"
+                        label="Email"
+                        variant="outlined"
+                        margin="normal"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
+                    />
 
-            >
-                <Form onSubmit={handleSubmitLogin} autoComplete="off" >
-         
-                    {/* <label htmlFor="email">Email</label>
-                    <Field id="email" name="email" /> */}
-
-                        <TextField 
-                            label="email"
-                            id="outlined-start-adornment"
-                            sx={{ m: 1, width: '25ch' }}
-                            InputProps={{
-                                startAdornment: <InputAdornment position="start">email</InputAdornment>,
-                            }}
-                        />
                         
-
-
-                    {/* <label htmlFor="password">Password</label>
-                    <Field id="password" name="password" /> */}
-                        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" >
+                    <FormControl variant="outlined" fullWidth margin="normal">
                         <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
                         <OutlinedInput
-                            id="outlined-adornment-password"
+                            id="password"
+                            name="password"
                             type={showPassword ? 'text' : 'password'}
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.password && Boolean(formik.errors.password)}
                             endAdornment={
                                 <InputAdornment position="end">
                                     <IconButton
@@ -92,17 +96,22 @@ export const LoginForm = () => {
                             }
                             label="Password"
                         />
+                        {formik.touched.password && formik.errors.password && (
+                            <div>{formik.errors.password}</div>
+                        )}
                     </FormControl>
 
-
-
-                        <Button variant="contained" endIcon={<SendIcon />}>
-                            Submit  </Button>
-                       
-                    {/* <button type="submit">Submit</button> */}
-                </Form>
-            </Formik>
+                    <Button type="submit" variant="contained" endIcon={<SendIcon />} fullWidth>
+                        Submit
+                    </Button>
+                </form>
             </div>
-        </Box>
+        
     )
 }
+
+
+
+
+
+
