b96a1750cd7c5e051ff33a18740422a85b948bd1
HannaNikola <h.nikolaienko32@gmail.com>
2024-01-25 14:12:35 +0100

    update

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,4 +1,4 @@
 import { Link } from "react-router-dom";
 export default function NotFoundPage() {
-    return <div>Sorry that page not found :( Please move to home page <Link to='/'>Home Page</Link></div>;
+    return <div>Sorry this page not found :( Please move to <Link to='/'>Home Page</Link></div>;
 }
